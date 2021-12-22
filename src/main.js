2b1d66f19c7374f566c061d55fefa38be7e7f9a8
aladin-jridi <aladinjridi007@gmail.com>
2021-12-23 00:17:34 +0100

    fix page product front

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { createApp } from 'vue' 
+import App from './App.vue' 
+import {createRouter , createWebHashHistory} from 'vue-router'
+ import Product from './components/Product.vue'
+  import HomePage from './components/HomePage.vue'
+//    import Login from './components/Login.vue'  
+   const routes = [  
+        { path: "/", name: "HomePage", component: HomePage },  
+    { path: "/Product", name: "product", component: Product }  
+    //  { path: "/login", name: "Login", component: Login } 
+    ];
+       const router = createRouter({   history: createWebHashHistory(process.env.BASE_URL),   routes, });
+         createApp(App).use(router).mount("#app")
