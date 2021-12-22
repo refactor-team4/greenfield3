44f701e009e3e55a07470257e270307536e8f275
AhmedBouafif <ahmedbouafif500@gmail.com>
2021-12-22 17:50:47 +0100

    home page front

---
+++ src/components/HomePage.vue
@@ -0,0 +1,245 @@
+<template>
+<body>
+
+<header class="nav-menu fixed">
+ <nav class="navbar normal transparent">
+  <div class="container-fluid">
+   <div class="navbar-header">
+    <a class="navbar-brand" href="index.html">
+      <img src="assets/img/logo.png" alt="Mold Discover Html Template">
+    </a>
+    
+  </div>
+
+  <div class=" collapsed" >
+     <ul class="nav">
+<li class="dropdown">
+    <a href="home_slider.html">Home</a>
+</li>
+<li>
+<a href="location_archive.html">Events</a>
+</li>
+<li class="dropdown">
+<a href="#" >.....</a>
+</li>
+<li>
+<a href="blog_list2.html" >Blog</a>
+</li>
+<li> 
+		<a href="login_page.html">Sign In</a>
+	</li>
+ </ul> 
+  </div>
+</div>
+</nav>
+</header>
+	
+	<div class="carousel slide carousel-fade full-height stick-top" id="carousel">
+		<div class="carousel-inner" role="listbox">
+			<div class="item with-overlay active" style="background-image: url('assets/img/trip_detail/4.jpg');">
+				<div class="carousel-caption center-txt">
+					<h1 class="main-header">Enjoy Adventure</h1>	
+					<hr>
+					<p class="sub-header">Wander Exotic places and without getting lost.</p>
+					<a href="#" class="btn btn-lg btn-primary hvr-sweep-to-right">Explore Trips</a>
+				</div>
+			</div>
+			<div class="item with-text-box with-overlay" style="background-image: url('assets/img/trip_detail/5.jpg');">
+				<div class="carousel-caption center-txt">
+					<h1 class="main-header">Outdoor Experience</h1>	
+					<hr>
+					<p class="sub-header">Wander Exotic places and without getting lost.</p>
+					<a href="#" class="btn btn-lg btn-primary hvr-sweep-to-right">Explore Trips</a>
+				</div>
+			</div>
+			<div class="item with-overlay" style="background-image: url('assets/img/trip_detail/6.jpg');">
+				<div class="carousel-caption center-txt">
+					<h1 class="main-header">Lost Joy</h1>	
+					<hr>
+					<p class="sub-header">Wander Exotic places and without getting lost.</p>
+					<a href="#" class="btn btn-lg btn-primary hvr-sweep-to-right">Explore Trips</a>
+				</div>
+			</div>
+
+		</div>
+
+		<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+			<span class="icon-arr-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+			<span class="icon-arr-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+
+		<ol class="carousel-indicators">
+			<li data-target="#carousel" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel" data-slide-to="1"></li>
+			<li data-target="#carousel" data-slide-to="2"></li>
+		</ol>
+	</div>	
+
+	
+
+	<div class="banner">
+		<div class="container-fluid" id="bobi">
+			<div class="section-title center">
+  <h3 id="bobii">Featured Guides</h3>
+</div>
+<div class="row team">
+  <div class="col-sm-6 col-md-2">
+    <div class="member" id='bob'>
+      <div class="image">
+        <img src="assets/img/team/1.jpg" alt="">
+      </div>
+      <h4 class="name">Kim L. Burney</h4>
+      <h5 class="detail">Guide/Writer</h5>
+      <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean </p>
+      <div class="social">
+        <a href="https://www.facebook.com/profile.php?id=100005139885531" class="icon icon-facebook"></a>
+        <a href="#" class="icon icon-twitter"></a>
+        <a href="#" class="icon icon-google"></a>
+        <a href="#" class="icon icon-rss"></a>
+      </div>
+    </div>
+  </div>
+	<div class="col-sm-6 col-md-2">
+	<div class="member" id='bob'>
+	<div class="image">
+	<img src="assets/img/team/1.jpg" alt="">
+	</div>
+		<h4 class="name">Kim L. Burney</h4>
+		<h5 class="detail">Guide/Writer</h5>
+		<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean </p>
+		<div class="social">
+		<a href="https://www.facebook.com/profile.php?id=100005139885531" class="icon icon-facebook"></a>
+		<a href="#" class="icon icon-twitter"></a>
+		<a href="#" class="icon icon-google"></a>
+		<a href="#" class="icon icon-rss"></a>
+		</div>
+	</div>
+	</div>
+	<div class="col-sm-6 col-md-2">
+		<div class="member" id='bob'>
+		<div class="image">
+		<img src="assets/img/team/1.jpg" alt="">
+		</div>
+		<h4 class="name">Kim L. Burney</h4>
+		<h5 class="detail">Guide/Writer</h5>
+		<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean </p>
+		<div class="social">
+			<a href="https://www.facebook.com/profile.php?id=100005139885531" class="icon icon-facebook"></a>
+			<a href="#" class="icon icon-twitter"></a>
+			<a href="#" class="icon icon-google"></a>
+			<a href="#" class="icon icon-rss"></a>
+		</div>
+		</div>
+	</div>
+  <div class="col-sm-6 col-md-2">
+    <div class="member"  id='bob'>
+      <div class="image">
+        <img src="assets/img/team/2.jpg" alt="">
+      </div>
+      <h4 class="name">Shing Ch'in</h4>
+      <h5 class="detail">Tour Guide</h5>
+      <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean </p>
+      <div class="social">
+        <a href="#" class="icon icon-facebook"></a>
+        <a href="#" class="icon icon-twitter"></a>
+        <a href="#" class="icon icon-google"></a>
+        <a href="#" class="icon icon-rss"></a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-2">
+    <div class="member" id='bob'>
+      <div class="image">
+        <img src="assets/img/team/3.jpg" alt="">
+      </div>
+      <h4 class="name">Pitez Maltr</h4>
+      <h5 class="detail">City Guide</h5>
+      <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean </p>
+      <div class="social">
+        <a href="#" class="icon icon-facebook"></a>
+        <a href="#" class="icon icon-twitter"></a>
+        <a href="#" class="icon icon-google"></a>
+        <a href="#" class="icon icon-rss"></a>
+      </div>
+    </div>
+  </div>
+</div>	
+		</div>
+	</div>
+
+
+
+	<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-7 col-md-3">
+				<h3>Mold Discover</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, quia, architecto? A, reiciendis eveniet! Esse est eaque adipisci natus rerum laudantium accusamus magni.</p>
+			</div>
+			<div class="col-sm-5 col-md-2">
+				<h3>Quick Link</h3>
+				<ul>
+					<li>Holiday Package</li>
+					<li>Summer Adventure</li>
+					<li>Bus and Trasnportation</li>
+					<li>Ticket and Hotel Booking</li>
+					<li>Trek and Hikings</li>
+				</ul>
+			</div>
+			<div class="col-sm-7 col-md-4">
+				<h3>Newsletter Signup</h3>
+				<p>Subscribe to our weekly newsletter to get news and update</p>
+				<br>
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Your Email">
+					<div class="input-group-btn">
+						<button class="btn btn-primary">Subscribe</button>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-5 col-md-2">
+				<h3>Contact Info</h3>
+				<ul>
+					<li>Mold Discover</li>
+					<li>info@moldthemes.com</li>
+				</ul>
+				<div class="clearfix">
+					<div class="social-icon-list">
+						<ul>
+							<li><a href="https://twitter.com/moldthemes" class="icon-twitter"></a></li>
+							<li><a href="mailto:info@moldthemes.com" class="icon-mail"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copy"><span>&copy;</span> Copyright Mold Discover, 2017</div>
+</footer>
+
+
+
+</body>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
