1afad05bb46ce68b8dfef7de73000cc594735312
aydisaddem <sdouma91@gmail.com>
2021-12-23 10:16:31 +0100

    getting updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Feed />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Feed from '@/components/Feed.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Feed
   }
 }
 </script>
